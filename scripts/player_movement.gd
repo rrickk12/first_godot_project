2b056066ac32e68cf1d6ae91123bc06657e908cb
Henrique Machado Gonçalves <henriquemg93@gmail.com>
2025-10-05 19:58:16 -0300

    First commit

---
+++ scripts/player_movement.gd
@@ -0,0 +1,36 @@
+extends CharacterBody2D
+
+const SPEED = 200.0
+const JUMP_VELOCITY = -150.0
+
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+# If your node is named differently, update "$AnimatedSprite2D" to match.
+
+func _physics_process(delta: float) -> void:
+	# Add gravity
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+		if sprite.animation != "jump":
+				sprite.play("jump")
+		
+	# Jump
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		if sprite.animation != "jump":
+				sprite.play("jump")
+	# Horizontal movement
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+		# Flip sprite depending on direction
+		sprite.flip_h = direction < 0
+		# Play walking animation if not already playing
+		if sprite.animation != "walk" and is_on_floor():
+			sprite.play("walk")
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		# Idle animation when not moving
+
+		if sprite.animation != "idle" and is_on_floor():
+			sprite.play("idle")
+	move_and_slide()
